8d94d5d858200187f73611ad16d5389b6a667be8
Pavel Prokofyev <feorand@gmail.com>
2016-10-10 11:50:16 +0500

    Created Text class for managing input text Created basic test for it

--- RSVPReader/Text.swift
+++ RSVPReader/Text.swift
@@ -9,6 +9,13 @@
 import Foundation
 
 class Text {
-    static let sampleText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin tincidunt odio eget posuere egestas. Curabitur rhoncus nisl mi, vel efficitur sapien posuere pellentesque. Suspendisse potenti. Proin quis fringilla urna, vel ultricies eros. Etiam nisl felis, mollis sed blandit et, congue quis magna. Nam venenatis erat ut accumsan placerat. Cras quis augue purus. In id ex lobortis, eleifend arcu id, aliquet lacus. Nunc non ultrices ligula, quis tristique augue. Ut est massa, lacinia eget porttitor vel, pretium pellentesque massa. Nullam vestibulum dictum tortor et facilisis."
+    
+    let words: [String]
+    
+    var currentWord = 0
+    
+    init(fromSource text:String) {
+        words = text.components(separatedBy: " ")
+    }
     
 }
