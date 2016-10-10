5638d720a2cddc72084aa9f2c1181ca28a3cad9f
Pavel Prokofyev <feorand@gmail.com>
2016-10-10 11:27:12 +0500

    Added Text class for managing operations with text

---
+++ RSVPReader/Text.swift
@@ -0,0 +1,14 @@
+//
+//  Text.swift
+//  RSVPReader
+//
+//  Created by Pavel Prokofyev on 10.10.16.
+//  Copyright © 2016 pavelprokofyev. All rights reserved.
+//
+
+import Foundation
+
+class Text {
+    static let sampleText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin tincidunt odio eget posuere egestas. Curabitur rhoncus nisl mi, vel efficitur sapien posuere pellentesque. Suspendisse potenti. Proin quis fringilla urna, vel ultricies eros. Etiam nisl felis, mollis sed blandit et, congue quis magna. Nam venenatis erat ut accumsan placerat. Cras quis augue purus. In id ex lobortis, eleifend arcu id, aliquet lacus. Nunc non ultrices ligula, quis tristique augue. Ut est massa, lacinia eget porttitor vel, pretium pellentesque massa. Nullam vestibulum dictum tortor et facilisis."
+    
+}
